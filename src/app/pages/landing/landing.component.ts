bec382e99bd514c0c8fcc2d6aa09f41419de5192
Mena <rmena@sistecredito.com>
2025-09-11 22:38:27 -0500

    fix: Correct import statement for PropertyListComponent in landing component

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PropertyListComponent } from '../property-list/property-list.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [prper],
+  imports: [PropertyListComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
