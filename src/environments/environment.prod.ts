c249f6cdc44f46ccf6bc9a55ab4b44b07363c74a
Juan José Sierra <juanjosesierram@gmail.com>
2025-09-15 21:24:16 -0500

    Deploy to Azure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://praevon-core-service.azurewebsites.net/api/core-service/v1'
+  apiUrl: 'https://praevon-core-service.azurewebsites.net/api/core-service/v1',
+  defaultPageLimit: 12,
+  authTokenKey: 'authToken',
+  currentUserKey: 'currentUser'
 };
